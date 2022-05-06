fe78635f4b4e2b4fb9dd417ed780f404d74d9b8a
mstars_lab2_08 <mstars_lab2_08@mstars-lab2-08s-iMac.local>
2022-05-06 18:01:42 +0800

    commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const port = 3000;
+const router = express.Router();
+app.use("/", router);
+
+router.get("/", function (req, res) {
+  fs.readFile("./data/book.json", (error, data) => {
+    if (error) {
+      throw error;
+    } else {
+      const booksarr = JSON.parse(data);
+      const titles = booksarr.books.map((e) => {
+        return e.title;
+      });
+      let randombooks = [];
+      for (i = 0; i < 1000; i++) {
+        if (randombooks.length < 3) {
+          let random = titles[Math.floor(Math.random() * titles.length)];
+          if (!randombooks.includes(random)) {
+            randombooks.push(random);
+          }
+          //   randombooks += `<li>${random}</li>`;
+        }
+      }
+
+      res.send(randombooks);
+    }
+  });
+});
+app.listen(port);
